240580351db73cf0297538fa8dafd055f2f4c8fb
Shah-dev <zaighumshahh@gmail.com>
2026-09-01 15:25:58 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,15 @@
 window.VOXORA_CONFIG = {
   supabaseUrl: "https://jbnrwnkwtgpilxqdmglf.supabase.co",
-  supabasePublishableKey: "sb_publishable_SWTZbHpN5aBcSIfVC9vKbg_noqnEfSr",
+
+  supabasePublishableKey: "APNI_PUBLISHABLE_KEY_YAHAN_RAKHO",
+
   tts: {
     baseUrl: "https://bubbly-cooperation-production-5157.up.railway.app",
     generatePath: "/generate",
     voicesPath: "/voices",
     healthPath: "/"
   },
+
   assistant: {
     endpoint: ""
   }
